0f59619b3c8afa20d92c9863298cfeedaa08dede
Jenny Hong <jennyhong@unknown60334b0858da.gateway.2wire.net>
2013-11-28 22:08:13 -0800

    trying to get eventListeners to work

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,31 @@
 function(window, document, undefined) {
 
+  var DOGE_URL = '/dogify/';
+  var STATUS_OK = 200;
+  var REQUEST_DONE = 4;
+
   var dogifyButton = document.getElementById('dogify');
+  var toDogify = document.getElementById('user-input');
+  var outputSpace = document.getElementById('result');
+
+  dogifyButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    console.log('button pressed');
+    var request = new XMLHttpRequest();
+    console.log(toDogify.value);
+    var URIinput = encodeURIComponent(toDogify.value);
+    request.open('GET', DOGE_URL + URIinput);
+    request.send();
+    request.addEventListener('readystatechange', function(event) {
+      if (this.readyState === REQUEST_DONE) {
+        if (this.status === STATUS_OK) {
+          var response = JSON.parse(this.responseText);
+          outputSpace.innerHTML = response;
+        } else {
+          // $('.error')[0].innerHTML = this.responseText;
+        }
+      }
+    });
+  });
 
 })(this, this.document);
